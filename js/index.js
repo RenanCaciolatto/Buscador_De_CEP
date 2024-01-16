5d7d464c04587503af336bc96a06a1bac1f44dcf
Renan Caciolatto <renancaciolatto@hotmail.com>
2024-01-16 12:19:40 -0300

    projeto completo

--- js/index.js
+++ js/index.js
@@ -1,20 +1,23 @@
 function buscarCEP() {
-    if (document.getElementById('cep').value != null || document.getElementById('cep').value != '') {
+  if (
+    document.getElementById("cep").value != null ||
+    document.getElementById("cep").value != ""
+  ) {
+    let cep = document.getElementById("cep").value;
+    const ajax = new XMLHttpRequest();
 
-        let cep = document.getElementById('cep').value;
-        const ajax = new XMLHttpRequest();
+    ajax.open("GET", "https://viacep.com.br/ws/" + cep + "/json/");
+    ajax.send();
 
-        ajax.open('GET', 'https://viacep.com.br/ws/'+cep+'/json/');
-        ajax.send();
+    ajax.onload = function () {
+      let obj = JSON.parse(this.responseText);
 
-        ajax.onload = function () {
-            let obj = JSON.parse(this.responseText);
-
-            document.getElementById('logradouro').innerHTML = 'Logradouro: ' + obj.logradouro;;
-            document.getElementById('bairro').innerHTML = 'Bairro: ' + obj.bairro;
-            document.getElementById('cidade').innerHTML = 'Cidade: ' + obj.localidade;
-            document.getElementById('estado').innerHTML = 'Estado: ' + obj.uf;
-            document.getElementById('ddd').innerHTML = 'DDD: ' + obj.ddd;
-        }
-    }
+      document.getElementById("logradouro").innerHTML =
+        "Logradouro: " + obj.logradouro;
+      document.getElementById("bairro").innerHTML = "Bairro: " + obj.bairro;
+      document.getElementById("cidade").innerHTML = "Cidade: " + obj.localidade;
+      document.getElementById("estado").innerHTML = "Estado: " + obj.uf;
+      document.getElementById("ddd").innerHTML = "DDD: " + obj.ddd;
+    };
+  }
 }
